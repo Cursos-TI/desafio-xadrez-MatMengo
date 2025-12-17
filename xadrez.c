b6796efb1d31b0e71de47933ee701e8d8eb76194
Matheus Mendes <matheusmsf81@gmail.com>
2025-12-17 12:49:16 +0000

    Nível aventureiro concluído

--- xadrez.c
+++ xadrez.c
@@ -2,30 +2,58 @@
 
 int main()
 {
+
+    int movimento = 1, i = 0; 
+
    // Mover a Torre 5 casas decimais para a direita
    for (int i = 0; i < 5; i++)
    {
-    printf("Torre: Direita\n"); // imprime a direção do movimento da Torre
+    printf("Torre: Direita - %d\n", movimento); // imprime a direção do movimento da Torre
+    movimento++;
    }
-    
-   // Mover o Bispo 5 casas na diagonal
-   int i = 0;
+   
+   printf("\n");
 
+   // Mover o Bispo 5 casas na diagonal
+    movimento = 1; // reinicia número de movimentos
+   
    do
    {
-    printf("Bispo: Acima + Direita\n");
+    printf("Bispo: Acima + Direita - %d\n", movimento);
     i++;
+    movimento++;
    } while (i < 5);
+
+   printf("\n");
 
    // Mover a Rainha oito casas para a esquerda
 
    i = 0; // reinicia i
+   movimento = 1; // reinicia número de movimentos
 
    while (i < 8)
    {
-    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda - %d\n", movimento);
     i++;
+    movimento++;
    }
+
+   printf("\n");
+
+   // movimentação do cavalo - 2 casa para baixo e 1 casa para esquerda
+  
+   movimento = 1; //reinicia varável movimento
+
+   for (i = 1; i > 0; i--)
+   {
+        while (movimento < 3)
+        {
+            printf("Cavalo: para baixo - %d \n", movimento);
+            movimento++;
+        }
+    printf("Cavalo: Esquerda - %d \n", i);
+   }
+   
  
     return 0;
 }
